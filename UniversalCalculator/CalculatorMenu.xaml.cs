677bd9975785ee7f5fede260b7202aefe1556d20
KingsonVictorio <Kingson.VICTORIO@studenttafesa.edu.au>
2025-03-15 18:06:12 +1030

    Added Trip Calculator Button which shall be developed in later stage

--- UniversalCalculator/CalculatorMenu.xaml.cs
+++ UniversalCalculator/CalculatorMenu.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -61,5 +62,12 @@ namespace Calculator
 		{
 			Application.Current.Exit();
 		}
-	}
+
+		private async void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var message = new MessageDialog("Trip Calculator C# code will be developed later.");
+			await message.ShowAsync();
+			return;
+		}
+    }
 }
